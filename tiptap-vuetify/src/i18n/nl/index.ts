0bac537ff596052e9740ddb0fa876b909bd01fa6
Maksimkus574 <kusenkovmaksim723@gmail.com>
2024-07-05 19:30:44 +0500

    Add files via upload

---
+++ tiptap-vuetify/src/i18n/nl/index.ts
@@ -0,0 +1,165 @@
+export default {
+  extensions: {
+    Blockquote: {
+      buttons: {
+        blockquote: {
+          tooltip: 'Citaatblok'
+        }
+      }
+    },
+    Bold: {
+      buttons: {
+        bold: {
+          tooltip: 'Dikgedrukt'
+        }
+      }
+    },
+    BulletList: {
+      buttons: {
+        bulletList: {
+          tooltip: 'Ongenummerde lijst'
+        }
+      }
+    },
+    Code: {
+      buttons: {
+        code: {
+          tooltip: 'Code'
+        }
+      }
+    },
+    CodeBlock: {
+      buttons: {
+        codeBlock: {
+          tooltip: 'Code blok'
+        }
+      }
+    },
+    History: {
+      buttons: {
+        undo: {
+          tooltip: 'Ongedaan maken'
+        },
+        redo: {
+          tooltip: 'Opnieuw uitvoeren'
+        }
+      }
+    },
+    HorizontalRule: {
+      buttons: {
+        horizontalRule: {
+          tooltip: 'Horizontale lijn'
+        }
+      }
+    },
+    Italic: {
+      buttons: {
+        italic: {
+          tooltip: 'Cursief'
+        }
+      }
+    },
+    OrderedList: {
+      buttons: {
+        orderedList: {
+          tooltip: 'Genummerde lijst'
+        }
+      }
+    },
+    Paragraph: {
+      buttons: {
+        paragraph: {
+          tooltip: 'Paragraaf'
+        }
+      }
+    },
+    Strike: {
+      buttons: {
+        strike: {
+          tooltip: 'Doorhalen'
+        }
+      }
+    },
+    Underline: {
+      buttons: {
+        underline: {
+          tooltip: 'Onderstrepen'
+        }
+      }
+    },
+    Heading: {
+      buttons: {
+        heading: {
+          tooltip: ({ level }) => 'Kop ' + level
+        }
+      }
+    },
+    Link: {
+      buttons: {
+        isActive: {
+          tooltip: 'Link wijzigigen'
+        },
+        notActive: {
+          tooltip: 'Link invoegen'
+        }
+      },
+      window: {
+        title: 'Link controle',
+        form: {
+          hrefLabel: 'URL'
+        },
+        buttons: {
+          close: 'Sluiten',
+          remove: 'Verwijderen',
+          apply: 'Toepassen'
+        }
+      }
+    },
+    Image: {
+      buttons: {
+        tooltip: 'Afbeelding'
+      },
+      window: {
+        title: 'Afbeelding toevoegen',
+        form: {
+          sourceLink: 'Afbeelding URL',
+          altText: 'Alternatieve tekst',
+          addImage: 'Afbeelding toevoegen'
+        },
+        imageUpload: {
+          instruction: 'Kies of sleep een of meerdere bestanden.'
+        },
+        buttons: {
+          close: 'Sluiten',
+          apply: 'Toepassen'
+        }
+      }
+    },
+    Table: {
+      buttons: {
+        table: {
+          tooltip: 'Tabel'
+        }
+      },
+      window: {
+        title: 'Tabellen',
+        form: {
+          rowsCount: 'Aantal rijen',
+          colsCount: 'Aantal kolommen',
+          withHeaderRow: 'Met koprij'
+        },
+        buttons: {
+          close: 'Sluiten',
+          apply: 'Toepassen'
+        }
+      }
+    },
+    TodoList: {
+      buttons: {
+        todoList: {
+          tooltip: 'Takenlijst'
+        }
+      }
+    }
+  }
+}
